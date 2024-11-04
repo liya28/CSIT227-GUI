9b42b729a745c75a1929f5c28e9783dae3599464
Aliyah Khaet Regacho <99122696+liya28@users.noreply.github.com>
2024-11-04 17:57:30 +0800

    Update FoodOrderingSystem.java

--- LA5_Regacho/src/FoodOrderingSystem/FoodOrderingSystem.java
+++ LA5_Regacho/src/FoodOrderingSystem/FoodOrderingSystem.java
@@ -1,7 +1,5 @@
 package FoodOrderingSystem;
 
-import SimpleCalculator.SimpleCalculator;
-
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -23,33 +21,43 @@ public class FoodOrderingSystem extends JFrame {
     FoodOrderingSystem() {
         JCheckBox[] foods = {cPizza, cBurger, cFries, cSoftDrinks, cTea, cSundae};
         int[] prices = {100, 80, 65, 55, 50, 40};
-        JRadioButton[] discounts = {rbNone, rb5, rb10, rb5};
+        JRadioButton[] discounts = {rbNone, rb5, rb10, rb15};
         double [] disc = {0, 0.05, 0.10, 0.15};
 
         btnOrder.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 double total = 0;
-                double current_total = 0;
+                double d = 0;
+                boolean chosen_food = false;
+                boolean choosen_disc = false;
 
                 for (int i = 0; i < foods.length; i++) {
                     if (foods[i].isSelected()) {
+                        chosen_food = true;
                         total += prices[i];
                     }
                 }
 
-                double d = 0;
-                for (int i = 0; i < discounts.length; i++) {
-                    if (discounts[i].isSelected()) {
-                        d = total * disc[i];
+                if (!chosen_food) {
+                    JOptionPane.showMessageDialog(null, "Choose one or more food!");
+                } else {
+                    for (int i = 0; i < discounts.length; i++) {
+                        if (discounts[i].isSelected()) {
+                            choosen_disc = true;
+                            d = total * disc[i];
+                        }
                     }
                 }
+                if (!choosen_disc) {
+                    JOptionPane.showMessageDialog(null, "Choose a discount option!");
+                } else {
+                    total -= d;
 
-                total -= d;
+                    String result = String.format("%.2f", total);
 
-                String result = String.format("%.2f", total);
-
-                JOptionPane.showMessageDialog(null, "The total price is Php " + result);
+                    JOptionPane.showMessageDialog(null, "The total price is Php " + result);
+                }
             }
         });
     }
